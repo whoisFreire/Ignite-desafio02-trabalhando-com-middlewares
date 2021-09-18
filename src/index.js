c6f044b06677565711158bec5a2ddb0a5377b7e3
Leonardo Freire <leonardofreire.russi@icloud.com>
2021-09-18 10:29:59 -0300

    desafio concluido

--- src/index.js
+++ src/index.js
@@ -10,19 +10,63 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json();
+  }
+
+  request.user = user;
+  return next();
+  
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(!user.pro && user.todos.length === 10){
+    return response.status(403).json();
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(user){
+    if(validate(id)){
+      user.todos.forEach(todo => {
+        if(todo.id === id){
+          request.todo = todo;
+          request.user = user;
+          return next();
+        }else{
+          return response.status(404).json({error: 'card nao encontrado'})
+        }
+      })
+    }else{
+      return response.status(400).json();
+    }
+  }
+  return response.status(404).json({error: 'usuario nao existe'});
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404).json();
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -64,7 +108,7 @@ app.patch('/users/:id/pro', findUserById, (request, response) => {
 
   return response.json(user);
 });
-
+ 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const { user } = request;
 
